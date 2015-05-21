1a5dcca8a279110d725a1b390c3e75b82a375d74
moscowart <moscowart99@gmail.com>
2015-05-21 21:26:48 +0300

    Lab1

--- lab1_mapper.py
+++ lab1_mapper.py
@@ -1,5 +1,5 @@
 import sys
 
 for line in sys.stdin:
-    for data in line.strip().split(','):
-        print '{0}\t{1}'.format(data[2], data[4])
+    data = line.strip().split(',')
+    print '{0}\t{1}'.format(data[2], data[4])
