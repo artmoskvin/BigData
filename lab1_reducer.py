c8b5abb26db28f903162564405178c1def34bcdc
Ubuntu <ubuntu@ip-172-31-21-200.eu-west-1.compute.internal>
2015-05-21 19:09:59 +0000

    DEBUGGED

--- lab1_reducer.py
+++ lab1_reducer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 
 prev_key = None
@@ -5,12 +7,12 @@ total = 0
 for line in sys.stdin:
     data = line.split('\t')
     key, count = data
-	If prev_key and key != prev_key:
-		print '{0}\t{1}'.format(prev_key, total)
-        total = 0
+    if prev_key and key != prev_key:
+	print '{0}\t{1}'.format(prev_key, total)
+    	total = 0
 
     prev_key = key
-	total += float(count)
+    total += float(count)
 
 if prev_key != None:
     print '{0}\t{1}'.format(prev_key, total)
